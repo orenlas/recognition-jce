544b5f1bdfa4db977c1e216f898ad48ae621269c
EC2 Default User <ec2-user@ip-172-31-59-145.ec2.internal>
2021-07-14 09:33:13 +0000

    first

--- helloworld/application.py
+++ helloworld/application.py
@@ -19,102 +19,15 @@ def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 
-@application.route('/calc/bit', methods=['GET'])
-def post_currency_bit():
-    return Response(json.dumps(get_bitcoin_index()), mimetype='application/json', status=200)
 
-
-def get_bitcoin_index():
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    response = requests.get(url).json()['bpi']['USD']
-    return response
-
-# return generic data
-@application.route('/get_generic', methods=['GET'])
-def get_generic_data():
-    return Response(json.dumps(generic_data), mimetype='application/json', status=200)
-
-
-# get example for multiplication
-# test get  
-# curl -i http://"localhost:5000/v1/multiply?first_num=12.1&second_num=12"
-
-@application.route('/v1/multiply', methods=['GET', 'POST'])
-def get_mult_res():
-    
-    first_num = request.args.get('first_num')
-    second_num = request.args.get('second_num')
-    res = float(first_num) * float(second_num) 
-    return Response(json.dumps({'multiplication result': res}), mimetype='application/json', status=200)
-
-
-
-
-# mock data
-currency_rate = {
-    'usd' : 3.3,
-    'pound' : 4.5,
-    'euro' : 4.8
-}
-
-#generic data
-generic_data = [
- 
-    {
-    "id":1,
-    "title": "wtf",
-    "body": "good will"
-    },
-    {
-    "id":2,
-    "title": "wtf2",
-    "body": "good will2"
-    }
-   ]
-
-
-@application.route('/get_forms', methods=['GET'])
-def get_frm():
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('forms')
-    # replace table scan
-    resp = table.scan()
-    print(str(resp))
-    return Response(json.dumps(str(resp['Items'])), mimetype='application/json', status=200)
-
-
-# curl -i -X POST -d'{"form_title":"form title1", "form_body":"where is it?", "form_type":"finance"}' -H "Content-Type: application/json" http://localhost:5000/set_form/frm4
-@application.route('/set_form/<frm_id>', methods=['POST'])
-def set_doc(frm_id):
-    
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('forms')
-    # get post data  
-    data = request.data
-    # convert the json to dictionary
-    data_dict = json.loads(data)
-    # retreive the parameters
-    form_body = data_dict.get('form_body','default')
-    form_title = data_dict.get('form_title','defualt')
-    form_type = data_dict.get('form_type', 'default')
-
-    item={
-    'form_id': frm_id,
-    'form_body': form_body,
-    'form_title': form_title, 
-    'form_type': form_type 
-     }
-    table.put_item(Item=item)
-    
-    return Response(json.dumps(item), mimetype='application/json', status=200)
     
     
 @application.route('/analyze/<bucket>/<image>', methods=['GET'])
-def analyze(bucket='my-upload-bucket-01', image='person.jpg'):
+def analyze(bucket='rekognition-jce', image='person.jfif'):
     return detect_labels(bucket, image)
 
 # curl localhost:5000/analyze/my-upload-bucket-01/person.jpg
-def detect_labels(bucket, key, max_labels=10, min_confidence=50, region="us-east-1"):
+def detect_labels(bucket, key, max_labels=10, min_confidence=70, region="us-east-1"):
     rekognition = boto3.client("rekognition", region)
     s3 = boto3.resource('s3', region_name = 'us-east-1')
 
